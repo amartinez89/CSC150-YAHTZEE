941faa8ba1094e4d4398cec42be21e73ea007cca
Aaron Martinez <amartinez@student.neumont.edu>
2013-06-03 15:00:09 -0600

    test

--- Yahtzee/src/Yahtzee.java
+++ Yahtzee/src/Yahtzee.java
@@ -1,9 +1,11 @@
 
 public class Yahtzee {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("hi yall");
 		System.out.println(sdgdsg);
+		System.out.println("test");
 	}
 
 }
